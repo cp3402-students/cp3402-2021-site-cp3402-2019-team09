bdcfc405f61191ad898db7120d1a0abdb32fd83f
allyssa <allyssa.olan@my.jcu.edu.au>
2021-05-26 20:01:25 +1000

    Add social links inside footer

--- customtheme/footer.php
+++ customtheme/footer.php
@@ -11,19 +11,31 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-            <?php printf('I think we should put social links here')?>
+<footer id="colophon" class="site-footer">
+    <div class="site-info">
+        <!--            --><?php //printf('I think we should put social links here')?>
 
-            <span class="sep"> | </span>
-            <img class="alignright"
-                 src=" <?php bloginfo('template_url');?>/images/joinNowButton.jpg" height="50" alt="JoinNowButton">'
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
+        <!--            <span class="sep"> | </span>-->
+        <img class="alignright"
+             src="<?php bloginfo('template_url'); ?>/images/joinNowButton.jpg" height="50" alt="JoinNowButton">
+        <a href="https://www.facebook.com/tsvjazzclub/">
+            <img class="alignleft" src="<?php bloginfo('template_url'); ?>/images/icon-fb.png" height="50"
+                 alt="Facebook">
+        </a>
+        <a href="https://www.instagram.com/tsvjazzclub/?hl=en">
+            <img class="alignleft" src="<?php bloginfo('template_url'); ?>/images/icon-ig.png" height="50"
+                 alt="Instagram">
+        </a>
+        <a href="mailto:townsvillejazz@icloud.com">
+            <img class="alignleft" src="<?php
+            bloginfo('template_url'); ?>/images/icon-email.png" height="50" alt="Mail">
+        </a>
+        <?php
+        /* translators: 1: Theme name, 2: Theme author. */
 
-                ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+        ?>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
